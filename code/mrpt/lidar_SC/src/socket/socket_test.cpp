41745c1a5b85b68d4a37c80c8d8a136ddcd13783
tbf <tbf@proscenic.cn>
2018-06-21 15:52:06 +0800

    socket

---
+++ code/mrpt/lidar_SC/src/socket/socket_test.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: socket_test.cpp
+    > Author: tangbf
+    > Mail: 
+    > Created Time: 2018年06月21日 星期四 15时35分12秒
+ ************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	cout << "socket" << endl;
+}
